9c60725385c7c49cbfa531f39420cef25793aa29
Sameh Mohamed <sameh@autojazari.com>
2017-02-07 12:46:56 -0500

    running prediction on AWS

--- P5-Vehicle-Detection/solution.py
+++ P5-Vehicle-Detection/solution.py
@@ -1,11 +1,19 @@
 import os
 import re
+import timeit
+
 import numpy as np
 import cv2
 
+from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 class VehicleClassifier(object):
+    
+    def __init__(self):
+        self.clf = SVC()
     
     def _os_walk(self, _dir):
         matches = []
@@ -99,7 +107,6 @@ class VehicleClassifier(object):
 
             hist_features = hist_features.reshape((hist_features.shape[0], 1))
 
-#             hog_feature = hog_features.reshape(hog_features.shape[0])
             # Append the new feature vector to the features list
             features.append(np.concatenate((spatial_features, hist_features, hog_features)))
         
@@ -108,7 +115,7 @@ class VehicleClassifier(object):
     
     
     
-    def load_training_data(self):
+    def load_data(self):
         def shuffle(x, y):
             perm = np.arange(len(x))
             np.random.shuffle(perm)
@@ -117,12 +124,20 @@ class VehicleClassifier(object):
 
             return (x, y)
     
-        vehicle_images = self.load_vehicle_images()[:10]
+        print("loading vehicle images")
+
+        vehicle_images = self.load_vehicle_images()
         
-        non_vehicle_images = self.load_non_vehicle_images()[:10]
+        print("load non-vehicle images")
+
+        non_vehicle_images = self.load_non_vehicle_images()
         
+        print("extract vehicle features")
+
         vehicle_features, y_vehicles = self.extract_features(vehicle_images, 1)
         
+        print("extract non-vehicle features")
+
         n_vehicle_features, y_n_vehicles = self.extract_features(non_vehicle_images, 0)
         
         assert len(vehicle_features) == len(y_vehicles), 'vehicle features and labels are imbalanced'
@@ -134,6 +149,10 @@ class VehicleClassifier(object):
         vehicle_features = vehicle_features[:count]
         
         n_vehicle_features = n_vehicle_features[:count]
+
+        y_vehicles = y_vehicles[:count]
+
+        y_n_vehicles = y_n_vehicles[:count]
         
         x = np.vstack((vehicle_features, n_vehicle_features)).astype(np.float64)
         
@@ -141,9 +160,43 @@ class VehicleClassifier(object):
         
         y = np.concatenate((y_vehicles, y_n_vehicles))
         
+        print("normalize features")
+
         x_scaler = StandardScaler().fit(x)
         
         scaled_x = x_scaler.transform(x)
         
-        return shuffle(scaled_x, y)
+        print("shuffle data")
+
+        X, Y = shuffle(scaled_x, y)
+
+        print("train / test split")
         
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+        
+        return (X_train, X_test, y_train, y_test)
+    
+    def calc_accuracy(self, y_true, y_pred):
+        return accuracy_score(y_true, y_pred)
+
+def main():
+    vc = VehicleClassifier()
+    
+    X_train, X_test, y_train, y_test = vc.load_data()
+    
+    # assert len(X) == len(Y), 'imbalanced data'
+    
+    print("train model")
+
+    vc.clf.fit(X_train, y_train)
+    
+    print("testing model")
+
+    y_pred = vc.clf.predict(X_test)
+    
+    print(vc.calc_accuracy(y_test, y_pred))
+
+
+
+if __name__ == '__main__':
+    timeit(main())
